d1fd462296aa9c58e6b93c1738c0562880780f72
sorano <rokudo.ananas.mukuro@gmail.com>
2021-10-07 17:22:08 +0300

    timer

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -19,18 +19,39 @@ const _createCell = (value) =>{
    return wrap
 }
 
-function gameCore(){
+(function gameCore(){
    const container = document.querySelector('.container')
    const startBtn = document.querySelector('.field_1__btn')
    const btnsSettings = document.querySelectorAll('.btn__option')
    const field1 = document.getElementById('field_1')
    const field2 = document.getElementById('field_2')
-   _setup()
+   const timeCounter = document.querySelector('#timer > span')
+   let time = 60;
 
-   function _setup(){
+   (function _setup(){
       show(100,field1)
       startBtn.addEventListener('click',startbtnHandler)
       btnsSettings.forEach(item => item.addEventListener('click',settingsHandler))
+   })()
+
+   function startGame(){
+      setInterval(decreaseTime,1000)
+   }
+   function finishGame(){
+   }
+   function setTime(value){
+      timeCounter.textContent = `00:${value}`
+   }
+   function decreaseTime(){
+      if(time === 0){
+         finishGame()
+      } else {
+         let current = --time
+         if(current < 10){
+            current = `0${current}`
+         }
+         setTime(current)
+      }
    }
 
    function show(ms,selector){
@@ -52,6 +73,12 @@ function gameCore(){
       field2.className = 'field_2'
       show(700,field3)
       container.append(field3)
+      setTimeout(() => {
+         document.getElementById('timer').style.display = 'flex'
+         timeCounter.textContent = '01:00'
+         startGame()
+      }, 1500);
+      
    }
 
    function startbtnHandler(){
@@ -60,5 +87,4 @@ function gameCore(){
       this.removeEventListener('click',startbtnHandler)
    }
 
-}
-gameCore()
+})()
